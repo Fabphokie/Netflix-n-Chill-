a114d5d389143cdc39ecadd1a218b3ed84175277
Mmaphokeng Senne <125877787+Fabphokie@users.noreply.github.com>
2024-08-28 11:24:29 +0200

    WIP

---
+++ netflix-n-chill/app/components/ProfileCard.js
@@ -0,0 +1,40 @@
+'use client'; // Ensure this directive is present
+
+import Image from 'next/image';
+import Link from 'next/link'; // Import Link from next/link
+import PropTypes from 'prop-types';
+
+export default function ProfileCard({ profile }) {
+  return (
+    <Link href={`/preview`} passHref>
+      <div
+        className="cursor-pointer"
+        role="button"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            window.location.href = `/previews/${profile.id}`;
+          }
+        }}
+      >
+        <Image
+          src={profile.image || '/default-image.png'}
+          alt={profile.name}
+          width={200}
+          height={200}
+          className="square-full border-4 border-gray-200"
+          onError={(e) => e.currentTarget.src = '/default-image.png'}
+        />
+        <h3 className="text-center mt-2 text-xl font-semibold">{profile.name}</h3>
+      </div>
+    </Link>
+  );
+}
+
+ProfileCard.propTypes = {
+  profile: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
---
+++ netflix-n-chill/app/pages/Preview/page.tsx
@@ -0,0 +1,15 @@
+// pages/previews.js
+
+import Link from 'next/link';
+
+export default function Previews() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black">
+      <h1 className="text-white text-4xl mb-8">Movie Previews</h1>
+      {/* Add movie preview content here */}
+      <Link href="/" passHref>
+        <a className="text-blue-500 mt-4">Back to Profiles</a>
+      </Link>
+    </div>
+  );
+}
