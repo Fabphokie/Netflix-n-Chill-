7ddc40a9ddd8fd84f42a2f168c20a4e6915bfc62
Mmaphokeng Senne <mmaphokengsenne@gmail.com>
2024-10-30 10:06:56 +0200

    wip

--- netflix-n-chill/app/components/ProfileCard.js
+++ netflix-n-chill/app/components/ProfileCard.js
@@ -6,26 +6,21 @@ import PropTypes from 'prop-types';
 
 export default function ProfileCard({ profile }) {
   return (
-    <Link href={`/preview`} passHref>
+    <Link href={`/previews/${profile.id}`} passHref>
       <div
-        className="cursor-pointer bg-black p-4 rounded-lg text-white flex flex-col items-center justify-center w-48 h-64" // Fixed dimensions for uniformity
+        className="cursor-pointer bg-black p-4 rounded-lg text-white flex flex-col items-center justify-center w-48 h-64 transition-transform transform hover:scale-105 hover:shadow-lg border-2 border-transparent hover:border-white" 
         role="button"
         tabIndex={0}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            window.location.href = `/previews/${profile.id}`;
-          }
-        }}
       >
         <Image
           src={profile.image || '/default-image.png'}
           alt={profile.name}
           width={150} 
           height={150} 
-          className=" border-4 border-gray-200 mb-2" // Use mb-2 for spacing below the image
+          className="mb-2" 
           onError={(e) => (e.currentTarget.src = '/default-image.png')}
         />
-        <h3 className="text-center mt-2 text-xl font-semibold">{profile.name}</h3>
+        <h3 className="text-center mt-2 text-xl font-normal transition-all duration-200 hover:font-semibold">{profile.name}</h3>
       </div>
     </Link>
   );
--- netflix-n-chill/app/page.tsx
+++ netflix-n-chill/app/page.tsx
@@ -7,12 +7,14 @@ export default function Home() {
   const profiles = [
     { id: '1', name: 'Profile 1', image: '/Darling.jpg' },
     { id: '2', name: 'Profile 2', image: '/Mmaphokeng.jpg' },
-    { id: '3', name: 'Profile 3', image: '/kids.jpg' },
-    { id: '4', name: 'Profile 4', image: '/kids.jpg' },
+    { id: '3', name: 'Profile 3', image: '/Darling.jpg' },
+    { id: '4', name: 'Profile 4', image: '/Mmaphokeng.jpg' },
   ];
 
   return (
-    <div className="flex bg-black min-h-screen items-center justify-center">
+    <div className="flex flex-col bg-black min-h-screen items-center justify-center text-white">
+      <h1 className="text-6xl mb-8">Who's watching?</h1>
+      
       <div className="flex space-x-4">
         {profiles.map(profile => (
           <ProfileCard key={profile.id} profile={profile} />
